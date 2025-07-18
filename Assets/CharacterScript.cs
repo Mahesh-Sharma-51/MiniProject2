01a9c9bbc4ef9ee787697165a354b69499db4ced
Mahesh Sharma <22bcs051@smvdu.ac.in>
2025-07-18 18:26:08 +0530

    Initial Unity Project Upload

--- Assets/CharacterScript.cs
+++ Assets/CharacterScript.cs
@@ -5,20 +5,31 @@ using UnityEngine;
 public class CharacterScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
+    public float flapStrength=5;
+    public LogicScriptt logic;
+    public bool birdIsAlive = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScriptt>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)==true)
+        if(Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
         {
-            myRigidbody.linearVelocity = Vector2.up * 5;
+            myRigidbody.linearVelocity = Vector2.up * flapStrength; 
         }
-        
+ 
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
+    }
+
 }
